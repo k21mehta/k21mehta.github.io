5c8cbc5d872eaf263d9b556a9cfcf2118a1a9aaa
Kritika Mehta <kritika.m0421@gmail.com>
2020-01-04 19:07:51 -0500

    initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+<script>
+$(function(){
+  // activate scrollspy on sections side navigation
+  $('body').scrollspy({ 
+    target: '.fixed-side-navbar',
+    offset: 200
+  });
+  
+  // smoothscroll on sidenav click
+  $('.fixed-side-navbar a').smoothScroll({
+    offset: -100    
+  });
+});
+</script>
